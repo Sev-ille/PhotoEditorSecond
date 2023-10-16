569637e6b68192edf186af5a534e2bbda04cceb9
Sev-ille <sevillebohn@gmail.com>
2023-10-16 12:08:18 -0500

    display layout

---
+++ main.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import Screen
+
+
+class PhotoEditorApp(App):
+    pass
+class Display(BoxLayout):
+    pass
+
+PhotoEditorApp().run()
