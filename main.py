964b3f2d960e62255381e0b67985e500668e7105
Sev-ille <sevillebohn@gmail.com>
2023-10-18 12:35:14 -0500

    display layout

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.image import Image
 from kivy.uix.screenmanager import Screen
+from PIL import Image
 
 
 class PhotoEditorApp(App):
     pass
 
-class Display(Screen,BoxLayout,Image):
+class Display(Screen,BoxLayout):
 
     def loadimage(self):
-        self.ids.img.source = self.ids.pic.source.text
+        self.ids.img.source = self.ids.pic.text
     def blue_filter(self):
         pic = self.ids.img.source
         img = Image.open(pic)
@@ -20,23 +20,32 @@ class Display(Screen,BoxLayout,Image):
                 pixel[x, y] = (pixel[x, y][0] == 255, pixel[x, y][1], pixel[x, y][2])
 
         img.save("Blue.png")
-    def red_filter(self,image):
+        self.ids.img.source = "Blue.png"
+    def red_filter(self):
+        pic = self.ids.img.source
+        image = Image.open(pic)
         pixel = image.load()
         for y in range(image.size[1]):
             for x in range(image.size[0]):
                 pixel[x, y] = (pixel[x, y][0] + 255, pixel[x, y][1], pixel[x, y][2])
 
         image.save("Red.png")
+        self.ids.img.source = "Red.png"
 
-    def invert(self,image):
+    def invert(self):
+        pic = self.ids.img.source
+        image = Image.open(pic)
         pixel = image.load()
         for y in range(image.size[1]):
             for x in range(image.size[0]):
                 pixel[x, y] = (255 - pixel[x, y][0], 255 - pixel[x, y][1], 255 - pixel[x, y][2])
 
         image.save("Inverse.png")
+        self.ids.img.source = "Inverse.png"
 
-    def sepia(self,image):
+    def sepia(self):
+        pic = self.ids.img.source
+        image = Image.open(pic)
         pixel = image.load()
         for y in range(image.size[1]):
             for x in range(image.size[0]):
@@ -49,8 +58,11 @@ class Display(Screen,BoxLayout,Image):
                 pixel[x, y] = (int(red1), int(green1), int(blue1))
 
         image.save("Sepia.png")
+        self.ids.img.source = "Sepia.png"
 
-    def pixelate(self,image, x, y, width, height):
+    def pixelate(self, x, y, width, height):
+        pic = self.ids.img.source
+        image = Image.open(pic)
         ing = image.open(image)
         pixels = ing.load()
         for _i in range(y, height + y, 20):
@@ -59,10 +71,13 @@ class Display(Screen,BoxLayout,Image):
                     for d in range(20):
                         pixels[_a + d, _i + s] = pixels[_a, _i]
         ing.save("pixelate.png")
+        self.ids.img.source
+        pass
 
-    def mirror_horizontal(self,image):
+    def mirror_horizontal(self):
+        pic = self.ids.img.source
+        ing = Image.open(pic)
         list = []
-        ing = image.open(image)
         pixels = ing.load()
         for y in range(ing.size[1]):
             list.append([])
@@ -73,6 +88,7 @@ class Display(Screen,BoxLayout,Image):
             for x in range(ing.size[0]):
                 pixels[x, y] = list[y][x]
         ing.save("mirror.png")
+        self.ids.img.source = "mirror.png"
     pass
 
 PhotoEditorApp().run()
